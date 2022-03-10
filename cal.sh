e5963789f747d431f1f72dab3e519631a5fa71f3
carollebertille <carolle.matchum@yahoo.com>
2022-03-10 01:46:57 -0600

    new change

--- cal.sh
+++ cal.sh
@@ -14,3 +14,18 @@ if [ -z $side ] || [ $side -eq 0 ]
   
 fi
 echo -e "\nSquare area is : $((side * side))"
+echo
+sleep 2
+
+echo -e "\nRECTANGLE AREA\n"
+echo -e "\nEnter length\n"
+read  l
+echo -e "\n Enter width\n"
+read w
+#if [ -z $side ] || [ $side -eq 0 ]
+#	 then
+#		   echo -e "\n Please enter valid number\n"
+#		     read side
+
+#fi
+echo -e "\nRectangle area is : $((l * w))"
